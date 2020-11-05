f57b7c1d0cac65b56ce03fb01e274a98468f074d
ZaenalMaestro <zaenal.official.1@gmail.com>
2020-11-05 20:40:30 +0800

    menyelesaikan algoritma naive bayes

--- naive_bayes.php
+++ naive_bayes.php
@@ -30,3 +30,58 @@ function totalStatusKelulusanTerlambat()
 }
 
 // hitung prior probability
+
+// priorProbability status kelulusan = tepat
+function priorStatusKelulusanTepat()
+{
+   $hasil = totalStatusKelulusanTepat() / totalDataTraining();
+   return $hasil;
+}
+
+// priorProbability status kelulusan = terlambat
+function priorStatusKelulusanTerlambat()
+{
+   $hasil = totalStatusKelulusanTerlambat() / totalDataTraining();
+   return $hasil;
+}
+
+// tahap 2
+
+function hitungConditional($key, $value)
+{
+   $hasil = [];
+   global $con;
+   $query = "SELECT COUNT($key) FROM mahasiswa WHERE $key = '$value' AND status_kelulusan";
+   for ($i = 0; $i < 2; $i++) {
+      if ($i == 0) {
+         $data = mysqli_query($con, $query .= "='tepat'");
+         $jumlahData = mysqli_fetch_row($data)[0];         
+         $hasil['tepat'] = (int)$jumlahData / totalStatusKelulusanTepat();
+      }else {
+         $data = mysqli_query($con, $query .= "='terlambat'");
+         $jumlahData = mysqli_fetch_row($data)[0];
+         $hasil['terlambat'] = (int) $jumlahData / totalStatusKelulusanTerlambat();;
+      }
+   }
+   return $hasil;
+}
+
+$data = [
+   "jenis_kelamin" => "laki-laki",
+   "status_mahasiswa" => "mahasiswa",
+   "status_pernikahan" => "belum",
+   "ipk_semester" => "2.70"
+];
+$hasil['jenis_kelamin'] = hitungConditional('jenis_kelamin', $data['jenis_kelamin']);
+$hasil['status_mahasiswa'] = hitungConditional('status_mahasiswa', $data['status_mahasiswa']);
+$hasil['status_pernikahan'] = hitungConditional('status_pernikahan', $data['status_pernikahan']);
+$hasil['ipk_semester'] = hitungConditional('ipk_semester', $data['ipk_semester']);
+
+$nilaiTepat = $hasil['jenis_kelamin']['tepat'] * $hasil['status_mahasiswa']['tepat'] * $hasil['status_pernikahan']['tepat'] * $hasil['ipk_semester']['tepat']*priorStatusKelulusanTepat();
+$nilaiTerlambat = $hasil['jenis_kelamin']['terlambat'] * $hasil['status_mahasiswa']['terlambat'] * $hasil['status_pernikahan']['terlambat'] * $hasil['ipk_semester']['terlambat']*priorStatusKelulusanTerlambat();
+var_dump($nilaiTepat, "-", $nilaiTerlambat);die;
+if ($nilaiTepat > $nilaiTerlambat) {
+   echo 'Status Kelulusan = "TEPAT';
+} else if ($nilaiTepat < $nilaiTerlambat) {
+   echo 'Status Kelulusan = "TERLAMBAT';
+}
